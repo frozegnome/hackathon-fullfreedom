2e83733490c97c920818f1879f7302cc824cdf8d
giuseppe <giuseppe.bini@gmail.com>
2015-10-17 13:44:01 -0400

    Add jquery for favoriting scholarships

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,17 @@
 (function ($) {
 	'use strict';
 
-	listScholar = Handlebars.compile(document.getElementById('list-scholar').innerHTML);
+	var fav = $('.favorite');
+
+	fav.on('click', function() {
+		if($(this).find('.liked').is(':visible')) {
+			$(this).find('.liked').hide();
+			$(this).find('.unliked').show();
+		}
+		else {
+			$(this).find('.unliked').hide();
+			$(this).find('.liked').show();
+		}
+	})
+
 })(jQuery)
